cd3cca0d28087f67347f253e619f81f02cb166ab
md areeb uddin <rmustafa632@gmail.com>
2022-10-22 10:04:29 +0530

    portfolio

---
+++ frontend-portfolio/src/Wrapper/AppWrap.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { NavigationDots, SocialMedia } from '../components'
+
+const AppWrap = (Components, idName, classNames) => function HOC() {
+  return (
+    <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+
+        <div className="app__wrapper app__flex">
+            <Components />
+
+            <div className="copyright">
+                <p className="p-text">
+                    @2022 AREEB
+                </p>
+            </div>
+        </div>
+        <NavigationDots active={idName} />
+    </div>
+  )
+}
+
+export default AppWrap
